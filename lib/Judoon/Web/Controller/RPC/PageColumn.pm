d7effa145ca3f2c44a21c0ada687c28f212678d6
Fitz Elliott <felliott@virginia.edu>
2012-02-13 15:51:28 -0500

    [mockapp] add page column controller + add page

---
+++ lib/Judoon/Web/Controller/RPC/PageColumn.pm
@@ -0,0 +1,40 @@
+package Judoon::Web::Controller::RPC::PageColumn;
+
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Judoon::Web::Controller::RPC'; }
+
+__PACKAGE__->config(
+    action => {
+        base => { Chained => '/rpc/page/id', PathPart => 'column', },
+    },
+    rpc => {
+        template_dir => 'page_column',
+        stash_key    => 'page_column',
+    },
+);
+
+
+override add_object => sub {
+    my ($self, $c, $params) = @_;
+    return $c->model('Users')->new_page_column($c->stash->{page}{id}, $params);
+};
+
+override get_object => sub {
+    my ($self, $c) = @_;
+    return $c->model('Users')->get_page_column($c->stash->{page_column}{id});
+};
+
+override edit_object => sub {
+    my ($self, $c, $params) = @_;
+    return $c->model('Users')
+        ->update_page_column($c->stash->{page_column}{id}, $params);
+};
+
+
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
