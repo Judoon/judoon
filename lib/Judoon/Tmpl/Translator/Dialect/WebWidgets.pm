055dc377d81bca39bda9e2873e428dba9f31a485
Fitz Elliott <felliott@virginia.edu>
2012-04-02 19:41:26 -0400

    [webwidgets] clean up and simplify code now that ids & format widgets are added in js

--- lib/Judoon/Tmpl/Translator/Dialect/WebWidgets.pm
+++ lib/Judoon/Tmpl/Translator/Dialect/WebWidgets.pm
@@ -29,8 +29,12 @@ method produce(\@nodes) {
 
     my ($count, $output) = 0;
     for my $node (@nodes) {
-        my $method = 'produce_' . $node->type;
-        $output .= $self->$method($node, $count);
+        my $method = 'produce_' . $node->type . '_tmpl';
+        my $node_tmpl = $self->$method();
+        my $node_output;
+        $self->tt->process(\$node_tmpl, {node => $node}, \$node_output, binmode => ':utf8')
+            or die 'Unable to process template: ' . $self->tt->error;
+        $output .= $node_output;
     }
 
     return $output;
@@ -112,36 +116,18 @@ method process_newline($widget) {
 
 # producer methods
 
-method produce_text($node, $count is alias) {
-# empty
-#     <div class="widget-object widget-type-text widget-inline btn-group">
-#       <input type="text" class="inner-small span2 w-dropdown widget-format-target" placeholder="Type text here" />[% INCLUDE format_dropdown %]
-#     </div>
-# filled
-#     <div id="widget_id_0" class="widget-object widget-type-text widget-inline btn-group">
-#      <input id="widget_format_id_0" class="inner-small span2 w-dropdown widget-format-target widget-format-sibling" placeholder="Type text here" value="Text" type="text"><a class="add-to btn dropdown-toggle" data-toggle="dropdown" href="#">
-#        <i class="icon-cog"></i>
-#      </a>
-#      <ul class="dropdown-menu">
-#        <li><a class="widget-action-bold"><i class="icon-bold"></i>Bold</a></li>
-#        <li><a class="widget-action-italic"><i class="icon-italic"></i>Italicize</a></li>
-#        <li><a class="widget-action-delete"><i class="icon-trash"></i>Delete</a></li>
-#      </ul>
-#    </div>
-
-    my $tmpl = <<'EOT';
+has produce_text_tmpl => (is => 'ro', isa => 'Str', lazy_build => 1);
+sub _build_produce_text_tmpl {
+    return <<'EOT';
 <div class="widget-object widget-type-text widget-inline btn-group">
   <input type="text" class="inner-small span2 w-dropdown widget-format-target widget-format-sibling [% FOREACH format IN node.formatting %]widget-formatting-[% format %] [% END %]" placeholder="Type text here" [% IF node %]value="[% node.value %]"[% END %] />
 </div>
 EOT
-
-    my $output = $self->fill_production_template($tmpl, {node => $node, count => $count});
-    $count++;
-    return $output;
 }
 
 
-method produce_variable($node, $count is alias) {
+has produce_variable_tmpl => (is => 'ro', isa => 'Str', lazy_build => 1);
+sub _build_produce_variable_tmpl {
 # empty
 #  <div class="widget-object widget-type-data widget-inline btn-group">
 #    <select class="w-dropdown widget-format-target">
@@ -162,49 +148,19 @@ method produce_variable($node, $count is alias) {
 #      <option value="unigene__human_homolog_">{Unigene (human homolog)}</option>
 #    </select>
 #  </div>
-
-
-    my $tmpl = <<'EOT';
+    return <<'EOT';
 <div class="widget-object widget-type-data widget-inline btn-group">
   <select class="w-dropdown widget-format-target widget-format-sibling [% FOREACH format IN node.formatting %]widget-formatting-[% format %] [% END %]">
     <option [% IF node %]value="[% node.name %]" selected="selected"[% END %]></option>
   </select>
 </div>
 EOT
-
-    my $output = $self->fill_production_template($tmpl, {node => $node, count => $count});
-    $count++;
-    return $output;
 }
 
 
-method produce_link($node, $count is alias) {
-# empty
-#  <div class="widget-object widget-type-link widget-inline btn-group">
-#    <a class="btn btn-edit-link"><i class="icon-pencil"></i> Edit link</a>
-#    <input type="hidden" class="widget-link-url-source widget-format-target" value="">
-#    <input type="hidden" class="widget-link-url-site"      value="">
-#    <input type="hidden" class="widget-link-url-prefix"    value="http://www.ncbi.nlm.nih.gov/gene/">
-#    <input type="hidden" class="widget-link-url-postfix"   value="">
-#    <input type="hidden" class="widget-link-url-datafield" value="protein_name">
-#    <input type="hidden" class="widget-link-label-type"  value="static">
-#    <input type="hidden" class="widget-link-label-value" value="Entrez">
-#  </div>
-# filled
-#  <div id="widget_id_5" class="widget-object widget-type-link widget-inline btn-group">
-#    <a class="btn btn-edit-link"><i class="icon-pencil"></i> Edit link</a>
-#    <input id="widget_format_id_5" class="widget-link-url-source widget-format-target" value="" type="hidden">
-#    <input class="widget-link-url-site" value="" type="hidden">
-#    <input class="widget-link-url-prefix" value="http://www.ncbi.nlm.nih.gov/gene/" type="hidden">
-#    <input class="widget-link-url-postfix" value="" type="hidden">
-#    <input class="widget-link-url-datafield" value="protein_name" type="hidden">
-#
-#    <input class="widget-link-label-type" value="static" type="hidden">
-#    <input class="widget-link-label-value" value="Entrez" type="hidden">
-#  </div>
-
-
-    my $tmpl = <<'EOT';
+has produce_link_tmpl => (is => 'ro', isa => 'Str', lazy_build => 1);
+sub _build_produce_link_tmpl {
+    return <<'EOT';
 <div class="widget-object widget-type-link widget-inline btn-group">
   <a class="btn btn-edit-link widget-format-sibling"><i class="icon-pencil"></i> Edit link</a>
   <input type="hidden" class="widget-link-url-source widget-format-target" value="">
@@ -216,37 +172,15 @@ method produce_link($node, $count is alias) {
   <input type="hidden" class="widget-link-label-value" value="[% node.label_value %]">
 </div>
 EOT
-
-    my $output = $self->fill_production_template($tmpl, {node => $node, count => $count});
-    $count++;
-    return $output;
 }
 
 
-method produce_newline($node, $count is alias) {
-# empty
-#   <div class="widget-object widget-type-newline-icon"><i class="icon-arrow-down"></i></div>
-#   <div class="widget-object widget-type-newline"></div>
-# filled
-#   <div id="widget_id_2" class="widget-object widget-type-newline-icon"><i class="icon-arrow-down"></i></div>
-#   <div id="widget_id_3" class="widget-object widget-type-newline"></div>
-
-    my $tmpl = <<'EOT';
+has produce_newline_tmpl => (is => 'ro', isa => 'Str', lazy_build => 1);
+sub _build_produce_newline_tmpl {
+    return <<'EOT';
 <div class="widget-object widget-type-newline-icon"><i class="icon-arrow-down"></i></div>
 <div class="widget-object widget-type-newline"></div>
 EOT
-
-    my $output = $self->fill_production_template($tmpl, {node => $node, count => $count});
-    $count++; $count++;
-    return $output;
-}
-
-
-method fill_production_template($tmpl, $stash) {
-    my $output;
-    $self->tt->process(\$tmpl, $stash, \$output, binmode => ':utf8')
-        or die 'Unable to process template: ' . $self->tt->error;
-    return $output;
 }
 
 
