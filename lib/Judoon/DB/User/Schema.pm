d74cd06673e2e5033a52a6817e67ebcb08ae3057
Fitz Elliott <felliott@virginia.edu>
2012-07-09 17:19:06 -0400

    [doc] add basic pod to Judoon::DB::User::Schema

--- lib/Judoon/DB/User/Schema.pm
+++ lib/Judoon/DB/User/Schema.pm
@@ -14,6 +14,17 @@ __PACKAGE__->load_namespaces;
 # Created by DBIx::Class::Schema::Loader v0.07024 @ 2012-05-15 21:44:31
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:0l6zHRqmj0fSyhLXxLAuog
 
+=pod
+
+=encoding utf8
+
+=head2 C<$VERSION>
+
+C<$VERSION> is very important for the DBIx::Class::Migration scripts
+to work
+
+=cut
+
 our $VERSION = 3;
 
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
