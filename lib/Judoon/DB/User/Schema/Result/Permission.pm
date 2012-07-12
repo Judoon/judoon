43a311cfa23319c969c73c77b33f079701b84a78
Fitz Elliott <felliott@virginia.edu>
2012-07-12 10:43:58 -0400

    [schema] update Permission with current dbicsl output (no functional changes)

--- lib/Judoon/DB/User/Schema/Result/Permission.pm
+++ lib/Judoon/DB/User/Schema/Result/Permission.pm
@@ -1,6 +1,9 @@
 use utf8;
 package Judoon::DB::User::Schema::Result::Permission;
 
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
 =head1 NAME
 
 Judoon::DB::User::Schema::Result::Permission
@@ -23,12 +26,13 @@ __PACKAGE__->table("permissions");
 
 =head1 ACCESSORS
 
-=head2 pk
+=head2 id
 
-  data_type: 'serial'
+  data_type: 'integer'
+  is_auto_increment: 1
   is_nullable: 0
 
-=head2 obj_fk
+=head2 obj_id
 
   data_type: 'integer'
   is_nullable: 0
@@ -68,10 +72,25 @@ __PACKAGE__->add_columns(
 
 __PACKAGE__->set_primary_key("id");
 
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<obj_id_unique>
+
+=over 4
+
+=item * L</obj_id>
+
+=back
+
+=cut
 
 __PACKAGE__->add_unique_constraint("obj_id_unique", ["obj_id"]);
 
 
+# Created by DBIx::Class::Schema::Loader v0.07025 @ 2012-07-12 10:42:15
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:It4yzrDVyyvCeL4wTLd0ng
 
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
 1;
