438e7c65caa458a9fa32259d3ebf143bf97598de
Fitz Elliott <felliott@virginia.edu>
2012-07-12 22:16:28 -0400

    [schema] add pod to Role::Result::HasPermissions

--- lib/Judoon/DB/User/Schema/Role/Result/HasPermissions.pm
+++ lib/Judoon/DB/User/Schema/Role/Result/HasPermissions.pm
@@ -3,6 +3,17 @@ package Judoon::DB::User::Schema::Role::Result::HasPermissions;
 use Moose::Role;
 use namespace::autoclean;
 
+
+=pod
+
+=encoding utf8
+
+=head2 register_permissions
+
+Register the permissions relationship with the composing class.
+
+=cut
+
 sub register_permissions {
     my ($class) = @_;
     $class->might_have(
