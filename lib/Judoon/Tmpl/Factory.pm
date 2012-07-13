dd1d3db10e0c105e73d46e24c235529173f72fbf
Fitz Elliott <felliott@virginia.edu>
2012-07-13 11:06:16 -0400

    [tmpl] add VarString support to ::Tmpl::Factory

--- lib/Judoon/Tmpl/Factory.pm
+++ lib/Judoon/Tmpl/Factory.pm
@@ -13,6 +13,7 @@ require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(
     build_node new_text_node new_variable_node new_link_node new_newline_node
+    new_varstring_node
 );
 
 
@@ -20,6 +21,7 @@ use Judoon::Tmpl::Node::Text;
 use Judoon::Tmpl::Node::Variable;
 use Judoon::Tmpl::Node::Link;
 use Judoon::Tmpl::Node::Newline;
+use Judoon::Tmpl::Node::VarString;
 
 
 =head2 build_node
@@ -31,25 +33,26 @@ representative hashref.
 
 sub build_node {
     my ($args) = @_;
-    return $args->{type} eq 'text'     ? new_text_node($args)
-        :  $args->{type} eq 'variable' ? new_variable_node($args)
-        :  $args->{type} eq 'link'     ? new_link_node($args)
-        :  $args->{type} eq 'newline'  ? new_newline_node($args)
+    return $args->{type} eq 'text'      ? new_text_node($args)
+        :  $args->{type} eq 'variable'  ? new_variable_node($args)
+        :  $args->{type} eq 'link'      ? new_link_node($args)
+        :  $args->{type} eq 'newline'   ? new_newline_node($args)
+        :  $args->{type} eq 'varstring' ? new_varstring_node($args)
         :      die "unrecognized node type: " . $args->{type};
 }
 
 
-=head2 new_text_node / new_variable_node / new_link_node / new_newline_node
+=head2 new_text_node / new_variable_node / new_link_node / new_newline_node / new_varstring_node
 
 Explicitly create new Judoon::Tmpl::Nodes
 
 =cut
 
-sub new_text_node     { Judoon::Tmpl::Node::Text->new(    $_[0]); }
-sub new_variable_node { Judoon::Tmpl::Node::Variable->new($_[0]); }
-sub new_link_node     { Judoon::Tmpl::Node::Link->new(    $_[0]); }
-sub new_newline_node  { Judoon::Tmpl::Node::Newline->new(      ); }
-
+sub new_text_node      { Judoon::Tmpl::Node::Text->new(     $_[0]); }
+sub new_variable_node  { Judoon::Tmpl::Node::Variable->new( $_[0]); }
+sub new_link_node      { Judoon::Tmpl::Node::Link->new(     $_[0]); }
+sub new_newline_node   { Judoon::Tmpl::Node::Newline->new(       ); }
+sub new_varstring_node { Judoon::Tmpl::Node::VarString->new($_[0]); }
 
 
 1;
