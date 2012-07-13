f56e91a578674c03fc121f4fdeae164ea8f323b9
Fitz Elliott <felliott@virginia.edu>
2012-07-13 08:45:47 -0400

    [tmpl] add pod to Tmpl::Node

--- lib/Judoon/Tmpl/Node.pm
+++ lib/Judoon/Tmpl/Node.pm
@@ -17,7 +17,25 @@ around 'pack' => sub {
     return $return;
 };
 
+
+=head2 type
+
+This method simply returns a string that describes the node type.
+This is a virtual method that must be implemented by subclasses.
+
+=cut
+
 sub type { ... }
+
+
+=head2 decompose
+
+This method takes a node and returns a simpler representation of it
+comprised only of Text and Variable nodes.  This is a virtual method
+that must be implemented by subclasses.
+
+=cut
+
 sub decompose { ... }
 
 __PACKAGE__->meta->make_immutable;
