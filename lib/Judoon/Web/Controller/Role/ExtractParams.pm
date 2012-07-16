622854de63204b5720793b680d2274f1ec6d27e8
Fitz Elliott <felliott@virginia.edu>
2012-07-16 16:00:37 -0400

    [web] add pod to Controller::Role::ExtractParams

--- lib/Judoon/Web/Controller/Role/ExtractParams.pm
+++ lib/Judoon/Web/Controller/Role/ExtractParams.pm
@@ -1,7 +1,47 @@
 package Judoon::Web::Controller::Role::ExtractParams;
 
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Judoon::Web::Controller::Role::ExtracrParams - get keyed parameters
+
+=head1 SYNOPSIS
+
+ package Judoon::Web::Controller::Dog;
+ with 'Judoon::Web::Controller::Role::ExtractParams';
+
+ sub add {
+    my ($self, $c) = @_;
+    my %new_dog_params = $self->extract_params('dog', $c->req->params);
+    $c->model('Dog')->create(\%new_dog_params);
+ }
+
+=head1 DESCRIPTION
+
+C<Judoon::Web::Controller::Role::ExtractParams> takes a string and a
+hashref of parameters and extracts any key/val pairs where the key
+begins with C<"$string\.">.  It returns a new hash with on;y those
+key/val pairs, and scrubs C<"string\."> from the keys. e.g.
+
+ $self->extract_params(
+     'dog',
+     {'dog.name' => 'fido', 'dog.age' => 3, bad => 1}
+ );
+ # returns (name => 'fido', age => 3);
+
+=cut
+
 use Moose::Role;
 use namespace::autoclean;
+
+=head2 B<C<extract_params($key, \%params)>>
+
+see L</DESCRIPTION>.
+
+=cut
 
 sub extract_params {
     my ($self, $key, $params) = @_;
