d7db4e90c290c34732843f1329eb04c1556d4a0d
Fitz Elliott <felliott@virginia.edu>
2012-07-19 18:44:33 -0400

    [web] add pod to new Login Controller

--- lib/Judoon/Web/Controller/Login.pm
+++ lib/Judoon/Web/Controller/Login.pm
@@ -16,8 +16,25 @@ use namespace::autoclean;
 BEGIN { extends 'Judoon::Web::Controller'; }
 
 
+=head2 C<B<not_required>>
+
+C<not_required> is a no-op chaining point for actions that do not
+require a login.
+
+=cut
+
 sub not_required : Chained('/') PathPart('') CaptureArgs(0) {}
-sub required     : Chained('/') PathPart('') CaptureArgs(0) {
+
+
+=head2 C<B<required>>
+
+C<required> is a chaining point for actions that require a user to be
+logged in.  If they are not, they will be sent to the login page, and
+their intended URL saved.  After login, they will be sent to the URL.
+
+=cut
+
+sub required : Chained('/') PathPart('') CaptureArgs(0) {
     my ($self, $c) = @_;
     if (!$c->user) {
         my $message = 'You need to login to view this page!';
@@ -27,19 +44,42 @@ sub required     : Chained('/') PathPart('') CaptureArgs(0) {
 }
 
 
+=head2 login
+
+Action for logging in. GET requests show the login page, POST requests
+attempt a login.
+
+=cut
+
 sub login :Chained('not_required') :PathPart('login') :Args(0) ActionClass('REST') {
     my ($self, $c) = @_;
     $c->stash->{template} = 'login/login.tt2';
 }
 
+
+=head2 login_GET
+
+If an already loged-in user hits this page again, they are sent to
+their overview.
+
+=cut
+
 sub login_GET {
     my ($self, $c) = @_;
-
     if ($c->user) {
         $self->go_here($c, '/user/edit', [$c->user->username]);
         $c->detach;
     }
 }
+
+
+=head2 login_POST
+
+Attepts to login the user with the supplied credentials.  If
+successful, redirects to the user's overview or the previously
+attempted url.  If unsuccessful, displays an error message.
+
+=cut
 
 sub login_POST {
     my ($self, $c) = @_;
@@ -55,10 +95,19 @@ sub login_POST {
     }
 }
 
+
 sub do_post_login_redirect {
     my ($self, $c) = @_;
     $c->res->redirect($self->redirect_after_login_uri($c));
 }
+
+
+=head2 login_redirect
+
+C<login_redirect> saves the requested url and redirects to the login
+page.
+
+=cut
 
 sub login_redirect {
     my ($self, $c, $message) = @_;
@@ -68,6 +117,14 @@ sub login_redirect {
     $c->detach;
 }
 
+
+=head2 redirect_after_login_uri
+
+returns the saved url if user was attempting to get to a protected
+page. Otherwise, sends user to their overview.
+
+=cut
+
 sub redirect_after_login_uri {
     my ($self, $c) = @_;
 
@@ -76,6 +133,12 @@ sub redirect_after_login_uri {
         : $c->uri_for_action('/user/edit', [$c->user->username]);
 }
 
+
+=head2 logout
+
+Action for logging out users.  Returns to index.
+
+=cut
 
 sub logout : Chained('/') PathPart('logout') Args(0) {
     my ($self, $c) = @_;
