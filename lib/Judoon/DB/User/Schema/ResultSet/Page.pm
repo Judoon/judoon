fd46b8e11e72d2f140eb66470979203b468e4281
Fitz Elliott <felliott@virginia.edu>
2012-08-02 17:11:40 -0400

    [schema] add for_dataset() method to RS:Page

--- lib/Judoon/DB/User/Schema/ResultSet/Page.pm
+++ lib/Judoon/DB/User/Schema/ResultSet/Page.pm
@@ -4,12 +4,33 @@ package Judoon::DB::User::Schema::ResultSet::Page;
 
 =encoding utf8
 
+=head1 NAME
+
+Judoon::DB::User::Schema::ResultSet::Page
+
+=head1 DESCRIPTION
+
+Custom ResultSet class for Pages
+
 =cut
 
 use Moo;
 use feature ':5.10';
 extends 'DBIx::Class::ResultSet';
 with 'Judoon::DB::User::Schema::Role::ResultSet::HasPermissions';
+
+=head1 METHODS
+
+=head2 B<C<for_dataset( $dataset )>>
+
+Filter C<Page>s to those belonging to a particular C<Dataset>.
+
+=cut
+
+sub for_dataset {
+    my ($self, $dataset) = @_;
+    return $self->search_rs({dataset_id => $dataset->id});
+}
 
 1;
 __END__
