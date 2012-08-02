23e26aaf037fff6e70c8277eb270baa8524d6640
Fitz Elliott <felliott@virginia.edu>
2012-08-02 17:49:22 -0400

    [schema] add RS::PageColumn with for_page() method

---
+++ lib/Judoon/DB/User/Schema/ResultSet/PageColumn.pm
@@ -0,0 +1,35 @@
+package Judoon::DB::User::Schema::ResultSet::PageColumn;
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Judoon::DB::User::Schema::ResultSet::PageColumn
+
+=head1 DESCRIPTION
+
+Custom ResultSet class for PageColumns
+
+=cut
+
+use Moo;
+use feature ':5.10';
+extends 'DBIx::Class::ResultSet';
+
+=head1 METHODS
+
+=head2 B<C<for_page( $page )>>
+
+Filter C<PageColumn>s to those belonging to a particular C<Page>.
+
+=cut
+
+sub for_page {
+    my ($self, $page) = @_;
+    return $self->search_rs({page_id => $page->id});
+}
+
+1;
+__END__
