2c04800b83c357e5019b868308b80195567b2e44
Fitz Elliott <felliott@virginia.edu>
2012-10-10 14:36:38 -0400

    [schema] update migration v1 populate script

--- share/migrations/_common/deploy/1/002-demo.pl
+++ share/migrations/_common/deploy/1/002-demo.pl
@@ -1,25 +1,10 @@
 #!/usr/bin/env perl
 
-# Author:       Fitz Elliott <felliott@virginia.edu>
-# Date Created: Wed Feb 29 16:09:18 2012
-# Description:  
-
 use strict;
 use warnings;
-use autodie;
-use feature ':5.12';
-
 use DBIx::Class::Migration::RunScript;
 
-main: {
-
-
-    migrate {
-
-        my $user_rs = shift
-            ->schema->resultset('User');
-
-        $user_rs->create({login => 'fge7z', name => 'Fitz Elliott'});
-    };
-}
-
+migrate {
+    my $user_rs = shift->schema->resultset('User');
+    $user_rs->create({login => 'felliott', name => 'Fitz Elliott'});
+};
