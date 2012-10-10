3ddf600c77882dc62345c5968e751b12ff737ea6
Fitz Elliott <felliott@virginia.edu>
2012-10-10 14:38:35 -0400

    [schema] update v5-6 migration script so it works

--- share/migrations/SQLite/upgrade/5-6/001-auto.sql
+++ share/migrations/SQLite/upgrade/5-6/001-auto.sql
@@ -4,9 +4,21 @@
 BEGIN;
 
 ;
-ALTER TABLE datasets ADD COLUMN tablename text NOT NULL;
 
-;
+DROP TABLE datasets;
+CREATE TABLE datasets (
+    id INTEGER PRIMARY KEY NOT NULL,
+    user_id integer NOT NULL,
+    name text NOT NULL,
+    notes text NOT NULL,
+    original text NOT NULL,
+    data text NOT NULL,
+    tablename text NOT NULL,
+    nbr_rows integer NOT NULL,
+    nbr_columns integer NOT NULL,
+    permission text NOT NULL DEFAULT 'private' CHECK (permission IN ('private', 'public')),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
 
 COMMIT;
 
