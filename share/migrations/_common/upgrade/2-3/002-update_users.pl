3842e6990f455a0aa85a7c16d9f8b0dfefad6550
Fitz Elliott <felliott@virginia.edu>
2012-10-10 14:37:09 -0400

    [schema] update v2-3 migration script to hash password

--- share/migrations/_common/upgrade/2-3/002-update_users.pl
+++ share/migrations/_common/upgrade/2-3/002-update_users.pl
@@ -3,19 +3,34 @@
 use strict;
 use warnings;
 use DBIx::Class::Migration::RunScript;
+use DBIx::Class::Migration::RunScript::Trait::AuthenPassphrase;
 
 use DateTime;
 
 migrate {
-    my $schema = shift->schema;
+    my $runscript = shift;
+
+    my $schema = $runscript->schema;
     $schema->resultset('Role')->populate([
         ['name'],
         ['admin'],
     ]);
-
     my $admin = $schema->resultset('Role')->find({name => 'admin'});
-    $schema->resultset('User')->find({username => 'fge7z'})->update({
-        password         => 'moomoo',
+
+    my $passphrase_args = {
+        passphrase       => 'rfc2307',
+        passphrase_class => 'BlowfishCrypt',
+        passphrase_args  => {
+            cost        => 8,
+            salt_random => 20,
+        },
+    };
+    my $encoded = $runscript->authen_passphrase(
+        $passphrase_args, 'moomoomoo',
+    );
+
+    $schema->resultset('User')->find({username => 'felliott'})->update({
+        password         => $encoded,
         password_expires => DateTime->now,
         name             => 'Fitz Elliott',
         email_address    => 'felliott@virginia.edu',
