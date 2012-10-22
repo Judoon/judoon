b28992349aae539bf6ef2de074e3ae64896b1ddc
Fitz Elliott <felliott@virginia.edu>
2012-10-22 14:56:19 -0400

    [error] add InvalidTemplate

---
+++ lib/Judoon/Error/InvalidTemplate.pm
@@ -0,0 +1,11 @@
+package Judoon::Error::InvalidTemplate;
+
+use Moo;
+use MooX::Types::MooseLike::Base qw(ArrayRef HashRef Str);
+extends 'Judoon::Error';
+
+has templates     => (is => 'ro', isa => ArrayRef[HashRef],);
+has valid_columns => (is => 'ro', isa => ArrayRef[Str],);
+
+1;
+__END__
