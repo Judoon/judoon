a8ca899b3c9f66024c63710e7caa6245dd075ba6
Fitz Elliott <felliott@virginia.edu>
2013-02-27 14:05:30 -0500

    [schema] add ordered_with_pages_and_pagecols to RS::Dataset

--- lib/Judoon/Schema/ResultSet/Dataset.pm
+++ lib/Judoon/Schema/ResultSet/Dataset.pm
@@ -46,4 +46,32 @@ sub with_pages {
 }
 
 
+=head2 ordered_with_pages_and_pagecols
+
+Fetch a list of datasets, ordered by their creation timestamp, while
+prefetching their pages and page columns.  Pages are ordered by
+creation timestamp, PageColumns are ordered by their sort field.
+
+In a future release of DBIC, we may be able to add dataset columns to
+this as well.  The order_by fields are very important for the current
+prefetch collapser, b/c related records must be contiguous.  This
+should also be fixed in the future DBIC release.
+
+=cut
+
+sub ordered_with_pages_and_pagecols {
+    my ($self) = @_;
+    return $self->search_rs(
+        undef,
+        {
+            prefetch => {'pages' => 'page_columns'},
+            order_by => [
+                {-asc => $self->me . 'created',},
+                {-asc => ['pages.dataset_id', 'pages.created',],},
+                {-asc => ['page_columns.page_id', 'page_columns.sort', ],},
+            ],
+        },
+    );
+}
+
 1;
