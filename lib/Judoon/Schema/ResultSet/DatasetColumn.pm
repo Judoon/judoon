d0475d581a524838e8196d5adcca9de1ac55222b
Fitz Elliott <felliott@virginia.edu>
2013-02-27 10:59:34 -0500

    [schema] add for_dataset_id method to RS::DatasetColumn

--- lib/Judoon/Schema/ResultSet/DatasetColumn.pm
+++ lib/Judoon/Schema/ResultSet/DatasetColumn.pm
@@ -18,15 +18,21 @@ __PACKAGE__->load_components('+DBIx::Class::Helper::ResultSet::Lookup');
 
 =head1 METHODS
 
-=head2 for_dataset( $dataset_id )
+=head2 for_dataset( $dataset ) / for_dataset_id( $dataset_id )
 
-Filter C<DatasetColumn> to those belonging to a particular C<Dataset>.
+Filter C<DatasetColumn> to those belonging to a particular
+C<Dataset>. C<for_dataset> takes a C<Dataset> row object, and
+C<for_dataset_id> takes a C<Dataset>'s C<id>.
 
 =cut
 
-sub for_dataset {
-    my ($self, $dataset) = @_;
-    return $self->search_rs({dataset_id => $dataset->id},{order_by => {-asc => 'sort'}});
+sub for_dataset { return $_[0]->for_dataset_id($_[1]->id); }
+
+sub for_dataset_id {
+    my ($self, $dataset_id) = @_;
+    return $self->search_rs(
+        { dataset_id => $dataset_id },
+        { order_by => {-asc => $self->me . 'sort'} });
 }
 
 
