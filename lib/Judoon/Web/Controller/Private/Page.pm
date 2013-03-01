09ec405c18f0d8e94116e1d1d9765a66e46c0fa6
Fitz Elliott <felliott@virginia.edu>
2013-02-28 22:13:42 -0500

    [web] remove references to unneeded View::Download::Plain
     * These are no-ops.  Just detach() instead.
    
     * Remove Judoon::View::Download::*
    
     * Remove Catalyst::View::Download from deps

--- lib/Judoon/Web/Controller/Private/Page.pm
+++ lib/Judoon/Web/Controller/Private/Page.pm
@@ -122,7 +122,6 @@ after object_GET => sub {
         $c->res->headers->header( "Content-Disposition" => "attachment; filename=judoon.$type" );
         my $archive = slurp($archive_path);
         $c->res->body($archive);
-        $c->forward('Judoon::Web::View::Download::Plain');
         $c->detach();
     }
     elsif ($view eq 'template') {
@@ -130,7 +129,7 @@ after object_GET => sub {
         my $name = $page->title;
         $c->res->headers->header( "Content-Disposition" => "attachment; filename=$name.json" );
         $c->res->body($page->dump_to_user);
-        $c->forward('Judoon::Web::View::Download::Plain');
+        $c->detach();
     }
 };
 
