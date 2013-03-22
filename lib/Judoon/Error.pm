fc68b966f8562b98a018ce444f29eab58663a540
Fitz Elliott <felliott@virginia.edu>
2013-03-22 13:08:08 -0400

    [error] add pod to ::Error & ::Error::Input

--- lib/Judoon/Error.pm
+++ lib/Judoon/Error.pm
@@ -1,5 +1,19 @@
 package Judoon::Error;
 
+=encoding utf8
+
+=head1 NAME
+
+Judoon::Error - base class for our exception hierarchy
+
+=head1 SYNOPSIS
+
+ if ($something_went_wrong) {
+     Judoon::Error->throw({message => "Oh no!"});
+ }
+
+=cut
+
 use Moo;
 extends 'Throwable::Error';
 
--- lib/Judoon/Error/Input.pm
+++ lib/Judoon/Error/Input.pm
@@ -1,7 +1,39 @@
 package Judoon::Error::Input;
 
+=encoding utf8
+
+=head1 NAME
+
+Judoon::Error::Input - User provides bad input exception
+
+=head1 SYNOPSIS
+
+ my $number = prompt_for_number();
+ if ($number !~ m/^\d+$/) {
+     Judoon::Error->throw({
+         message  => "That's not a number",
+         got      => $number,
+         expected => 'a number: qr{^\d+$}'
+     });
+ }
+
+=cut
+
 use Moo;
 extends 'Judoon::Error';
+
+
+=head1 ATTRIBUTES
+
+=head2 expected
+
+The expected value. Can be anything!
+
+=head2 got
+
+The received value. Can also be anything!
+
+=cut
 
 has 'expected' => (is => 'ro',);
 has 'got'      => (is => 'ro',);
