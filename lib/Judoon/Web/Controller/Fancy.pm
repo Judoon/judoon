4dfce2b4d87db430618d2d0b43b25bd213a62323
Fitz Elliott <felliott@virginia.edu>
2013-06-04 16:28:47 -0400

    more wip: make dynamic and static pages

--- lib/Judoon/Web/Controller/Fancy.pm
+++ lib/Judoon/Web/Controller/Fancy.pm
@@ -8,6 +8,7 @@ with qw(
     Judoon::Web::Controller::Role::ExtractParams
 );
 
+use Judoon::Tmpl;
 use Safe::Isa;
 use Try::Tiny;
 
@@ -21,17 +22,32 @@ sub yep : Chained('base') PathPart('') Args(0) {
 
 sub page_id : Chained('base') PathPart('page') CaptureArgs(1) {
     my ($self, $c, $page_id) = @_;
+    die 'Well, shit.' unless ($c->user);
     my $page = $c->user->my_pages->find({id => $page_id});
     $c->stash->{page}{object} = $page;
 }
 
 
-sub view : Chained('page_id') PathPart('') Args(0) {
+sub page_view : Chained('page_id') PathPart('') Args(0) {
     my ($self, $c) = @_;
+
+    my $page = $c->stash->{page}{object};
+
+    $c->stash->{dataset}{id} = $page->dataset->id;
+
+    $c->stash->{page_column}{list} = [$page->page_columns_ordered->hri->all];
+    for my $column (@{$c->stash->{page_column}{list}}) {
+        my $tmpl = Judoon::Tmpl->new_from_native($column->{template});
+        $column->{js_template} = $tmpl->to_jstmpl;
+    }
+
     $c->stash->{template} = 'fancy/page-view.tt2';
 }
 
-
+sub page_static : Chained('page_id') PathPart('static') Args(0) {
+    my ($self, $c) = @_;
+    $c->stash->{template} = 'fancy/page-view-static.tt2';
+}
 
 1;
 __END__
