bd1a4dbb9e1749598751c08d385426507aa71458
Fitz Elliott <felliott@virginia.edu>
2013-06-24 14:19:26 -0400

    dataTables display now works

--- root/static/js/app/app.js
+++ root/static/js/app/app.js
@@ -1,179 +1,117 @@
 var judoonApp = angular.module('judoon', ['ngSanitize']);
 
-    // judoonApp.directive('judoonTable', function() {
-    //     return function(scope, element, attrs) {
+    judoonApp.directive('judoonTable', function() {
+        return function(scope, element, attrs) {
+            scope.$watch('page', function() {
+                if (!scope.page.columns) {
+                    return;
+                }
 
-    //         // apply DataTable options, use defaults if none specified by user
-    //         var options = {};
-    //         if (attrs.judoonTable.length > 0) {
-    //             options = scope.$eval(attrs.myTable);
-    //         } else {
-    //             options = {
-    //                 "bAutoWidth": false,
-    //                 "bServerSide": true,
-    //                 "bProcessing" : true,
-    //                 "sDom": "R<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
-    //                 "sPaginationType": "bootstrap",
-    //                 "oLanguage": {
-    //                     "sLengthMenu": "_MENU_ records per page"
-    //                 }
-                    
-    //                 // "bStateSave": true,
-    //                 // "iCookieDuration": 2419200, /* 1 month */
-    //                 // "bJQueryUI": true,
-    //                 // "bPaginate": false,
-    //                 // "bLengthChange": false,
-    //                 // "bFilter": false,
-    //                 // "bInfo": false,
-    //                 //"bDestroy": true
-    //             };
-    //         }
+                // apply DataTable options, use defaults if none specified by user
+                var options = {
+                    "bAutoWidth": false,
+                    "bServerSide": true,
+                    "bProcessing" : true,
+                    "sPaginationType": "bootstrap"
+                };
 
-    //         // Tell the dataTables plugin what columns to use
-    //         // We can either derive them from the dom, or use setup from the controller
+                options["aoColumns"] = [];
+                for (idx in scope.page.columns) {
+                    options["aoColumns"][idx] = scope.page.columns[idx].title;
+                }
 
-    //         var explicitColumns = [];
-    //         element.find('th').each(function(index, elem) {
-    //             explicitColumns.push($(elem).text());
-    //         });
-    //         if (explicitColumns.length > 0) {
-    //             options["aoColumns"] = explicitColumns;
-    //         } else if (attrs.aoColumns) {
-    //             options["aoColumns"] = scope.$eval(attrs.aoColumns);
-    //         }
+                options["sAjaxSource"] = "/api/datasetdata/" + scope.page.dataset_id;
+                options["fnServerData"] = scope.getServerData;
 
-    //         // aoColumnDefs is dataTables way of providing fine control over column config
-    //         if (attrs.aoColumnDefs) {
-    //             options["aoColumnDefs"] = scope.$eval(attrs.aoColumnDefs);
-    //         }
-            
-    //         if (attrs.fnRowCallback) {
-    //             options["fnRowCallback"] = scope.$eval(attrs.fnRowCallback);
-    //         }
-
-    //         // apply the plugin
-    //         var dataTable = element.dataTable(options);
-
-            
-    //         // watch for any changes to our data, rebuild the DataTable
-    //         scope.$watch(attrs.aaData, function(value) {
-    //             var val = value || null;
-    //             if (val) {
-    //                 dataTable.fnClearTable();
-    //                 dataTable.fnAddData(scope.$eval(attrs.aaData));
-    //             }
-    //         });
-    //     };
-    // });
+                // apply the plugin
+                var dataTable = element.dataTable(options);
+            });
+        };
+    });
 
 
 
-// function Ctrl($scope) {
+function Ctrl($scope) {
 
-//     $scope.message = '';            
     
-//     $scope.myCallback = function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
-//         $('td:eq(2)', nRow).bind('click', function() {
-//             $scope.$apply(function() {
-//                 $scope.someClickHandler(aData);
-//             });
-//         });
-//         return nRow;
-//     };
-
-//     $scope.someClickHandler = function(info) {
-//         $scope.message = 'clicked: '+ info.price;
-//     };
+    $scope.columnDefs = [ 
+        { "mDataProp": "category", "aTargets":[0]},
+        { "mDataProp": "name", "aTargets":[1] },
+        { "mDataProp": "price", "aTargets":[2] }
+    ]; 
+  
     
-//     $scope.columnDefs = [ 
-//         { "mDataProp": "category", "aTargets":[0]},
-//         { "mDataProp": "name", "aTargets":[1] },
-//         { "mDataProp": "price", "aTargets":[2] }
-//     ]; 
+    $scope.sampleProductCategories = [
         
-//     $scope.overrideOptions = {
-//         "bStateSave": true,
-//         "iCookieDuration": 2419200, /* 1 month */
-//         "bJQueryUI": true,
-//         "bPaginate": true,
-//         "bLengthChange": false,
-//         "bFilter": true,
-//         "bInfo": true,
-//         "bDestroy": true
-//     };
-    
-    
-//     $scope.sampleProductCategories = [
-        
-//         {
-//             "name": "1948 Porsche 356-A Roadster",
-//                 "price": 53.9,
-//                   "category": "Classic Cars",
-//                   "action":"x"
-//               },
-//               {
-//                 "name": "1948 Porsche Type 356 Roadster",
-//                 "price": 62.16,
-//             "category": "Classic Cars",
-//                   "action":"x"
-//               },
-//               {
-//                 "name": "1949 Jaguar XK 120",
-//                 "price": 47.25,
-//             "category": "Classic Cars",
-//                   "action":"x"
-//               }
-//               ,
-//               {
-//                 "name": "1936 Harley Davidson El Knucklehead",
-//                 "price": 24.23,
-//             "category": "Motorcycles",
-//                   "action":"x"
-//               },
-//               {
-//                 "name": "1957 Vespa GS150",
-//                 "price": 32.95,
-//             "category": "Motorcycles",
-//                   "action":"x"
-//               },
-//               {
-//                 "name": "1960 BSA Gold Star DBD34",
-//                 "price": 37.32,
-//             "category": "Motorcycles",
-//                   "action":"x"
-//               }
-//            ,
-//               {
-//                 "name": "1900s Vintage Bi-Plane",
-//                 "price": 34.25,
-//             "category": "Planes",
-//                   "action":"x"
-//               },
-//               {
-//                 "name": "1900s Vintage Tri-Plane",
-//                 "price": 36.23,
-//             "category": "Planes",
-//                   "action":"x"
-//               },
-//               {
-//                 "name": "1928 British Royal Navy Airplane",
-//                 "price": 66.74,
-//             "category": "Planes",
-//                   "action":"x"
-//               },
-//               {
-//                 "name": "1980s Black Hawk Helicopter",
-//                 "price": 77.27,
-//             "category": "Planes",
-//                   "action":"x"
-//               },
-//               {
-//                 "name": "ATA: B757-300",
-//                 "price": 59.33,
-//             "category": "Planes",
-//                   "action":"x"
-//               }
+        {
+            "name": "1948 Porsche 356-A Roadster",
+                "price": 53.9,
+                  "category": "Classic Cars",
+                  "action":"x"
+              },
+              {
+                "name": "1948 Porsche Type 356 Roadster",
+                "price": 62.16,
+            "category": "Classic Cars",
+                  "action":"x"
+              },
+              {
+                "name": "1949 Jaguar XK 120",
+                "price": 47.25,
+            "category": "Classic Cars",
+                  "action":"x"
+              }
+              ,
+              {
+                "name": "1936 Harley Davidson El Knucklehead",
+                "price": 24.23,
+            "category": "Motorcycles",
+                  "action":"x"
+              },
+              {
+                "name": "1957 Vespa GS150",
+                "price": 32.95,
+            "category": "Motorcycles",
+                  "action":"x"
+              },
+              {
+                "name": "1960 BSA Gold Star DBD34",
+                "price": 37.32,
+            "category": "Motorcycles",
+                  "action":"x"
+              }
+           ,
+              {
+                "name": "1900s Vintage Bi-Plane",
+                "price": 34.25,
+            "category": "Planes",
+                  "action":"x"
+              },
+              {
+                "name": "1900s Vintage Tri-Plane",
+                "price": 36.23,
+            "category": "Planes",
+                  "action":"x"
+              },
+              {
+                "name": "1928 British Royal Navy Airplane",
+                "price": 66.74,
+            "category": "Planes",
+                  "action":"x"
+              },
+              {
+                "name": "1980s Black Hawk Helicopter",
+                "price": 77.27,
+            "category": "Planes",
+                  "action":"x"
+              },
+              {
+                "name": "ATA: B757-300",
+                "price": 59.33,
+            "category": "Planes",
+                  "action":"x"
+              }
           
-//         ];            
+        ];            
             
-// }
+}
--- root/static/js/app/controllers.js
+++ root/static/js/app/controllers.js
@@ -3,10 +3,37 @@
 /* Controllers */
 
 function PageCtrl($scope, $http) {
-  $scope.pageId = 49;
+    $scope.pageId = 49;
+    $scope.page = {};
 
-  $http.get('/api/page/'+$scope.pageId).success(function(data) {
-      $scope.page = data;
-  });
+    var init = $http.get('/api/page/'+$scope.pageId).success(function(data) {
+        $scope.page = data;
+        for (var idx in $scope.page.columns) {
+            $scope.page.columns[idx].compiled = Handlebars.compile(
+                $scope.page.columns[idx].template
+            );
+        }
+    });
 
+    $scope.getServerData = function ( sSource, aoData, fnCallback ) {
+        $.ajax( {
+            "dataType": "json",
+            "type": "GET",
+            "url": sSource,
+            "data": aoData,
+            "success": [
+                function(data) {
+                    var new_data = [];
+                    for (var i = 0; i < data.tmplData.length; i++) {
+                        new_data[i] = [];
+                        for (var j = 0; j < $scope.page.columns.length; j++) {
+                            new_data[i][j] = $scope.page.columns[j].compiled(data.tmplData[i]);
+                        }
+                    }
+                    data.aaData = new_data;
+                },
+                fnCallback
+            ],
+        } );
+    };
 }
