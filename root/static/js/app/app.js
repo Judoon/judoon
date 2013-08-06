74168c9218e1b6e8db0c63e246056a5165b574f5
Fitz Elliott <felliott@virginia.edu>
2013-08-06 13:36:34 -0400

    [jsapp] minor code cleanup in app.js

--- root/static/js/app/app.js
+++ root/static/js/app/app.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var judoonApp = angular.module('judoon', ['judoon.services','judoon.controllers','judoon.directives']);
+var judoonApp = angular.module(
+    'judoon',
+    ['ui.bootstrap','judoon.services','judoon.controllers','judoon.directives']
+);
 
 judoonApp.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true);
