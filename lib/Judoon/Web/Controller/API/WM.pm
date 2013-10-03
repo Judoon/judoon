31c1b430582cf86abe5f64f1c086bf10ed347d66
Fitz Elliott <felliott@virginia.edu>
2013-10-03 17:55:19 -0400

    [api] yet another redesign of the api

--- lib/Judoon/Web/Controller/API/WM.pm
+++ lib/Judoon/Web/Controller/API/WM.pm
@@ -23,6 +23,8 @@ BEGIN {extends 'Catalyst::Controller'; }
 use Judoon::API::Machine;
 use Module::Load;
 use HTTP::Response;
+use HTTP::Throwable::Factory qw(http_throw);
+
 
 =head1 Methods
 
@@ -47,8 +49,7 @@ sub wm {
 
 =head2 wm_base
 
-The base action for all other actions.  Puts the authenticated user in
-the stash.
+The base action for all other actions.
 
 =cut
 
@@ -57,9 +58,21 @@ sub wm_base : Chained('/api/base') PathPart('') CaptureArgs(0) {
 }
 
 
+=head1 Unauthenticated Actions
+
+These actions are available to all users and do not require any sort
+of authentication.
+
+ /users
+ /users/$username
+ /users/$username/datasets
+ /users/$username/pages
+ /public_datasets
+ /public_pages
+
 =head2 user_base / users / user_id / user
 
-Information about Judoon users.
+Public information about Judoon users.
 
 =cut
 
@@ -91,39 +104,144 @@ sub user : Chained('user_id') PathPart('') Args(0) ActionClass('FromPSGI') {
     );
 }
 
+=head2 public_datasets / public_pages
 
-=head2 dataset_base / datasets / dataset_id / dataset
-
-Authenticated users get full access to their datasets and read access
-to public datasets.
+Return lists of publicly available datasets and pages.
 
 =cut
 
-sub dataset_base : Chained('wm_base') PathPart('datasets') CaptureArgs(0) {
-    my ($self, $c) = @_;
-    $c->stash->{dataset_rs} = $c->model('User::Dataset')->public();
-}
-sub datasets : Chained('dataset_base') PathPart('') Args(0) ActionClass('FromPSGI') {
+sub public_datasets : Chained('wm_base') PathPart('public_datasets') Args(0) ActionClass('FromPSGI') {
     my ($self, $c) = @_;
     return $self->wm(
         $c, 'Judoon::API::Resource::Datasets', {
-            set      => $c->stash->{dataset_rs},
+            set      => $c->model('User::Dataset')->public(),
             writable => 0,
         }
     );
 }
+sub public_pages : Chained('wm_base') PathPart('public_pages') Args(0) ActionClass('FromPSGI') {
+    my ($self, $c) = @_;
+    return $self->wm(
+        $c, 'Judoon::API::Resource::Pages', {
+            set      => $c->model('User::Page')->public(),
+            writable => 0,
+        }
+    );
+}
+
+
+
+=head1 Authenticated Routes
+
+These routes require that the user be authenticated.  They return
+information about the authenticated user.
+
+ /user
+ /user/datasets
+ /user/pages
+
+=head2 authd_user_base / authd_user / authd_user_datasets / authd_user_pages
+
+Validate the authenticated user, then return information about
+themselves, their datasets, or their pages.
+
+=cut
+
+sub authd_user_base : Chained('wm_base') PathPart('user') CaptureArgs(0) {
+    my ($self, $c) = @_;
+    $c->stash->{authd_user} = $c->user->get_object if ($c->user);
+}
+sub authd_user : Chained('authd_user_base') PathPart('') Args(0) ActionClass('FromPSGI') {
+    my ($self, $c) = @_;
+    return $self->wm(
+        $c, 'Judoon::API::Resource::User', {
+            item     => $c->stash->{authd_user},
+            writable => 0,
+        }
+    );
+}
+sub authd_user_datasets : Chained('authd_user_base') PathPart('datasets') Args(0) ActionClass('FromPSGI') {
+    my ($self, $c) = @_;
+    return $self->wm(
+        $c, 'Judoon::API::Resource::Datasets', {
+            set      => $c->stash->{authd_user}->datasets_rs,
+            writable => 0,
+        }
+    );
+}
+sub authd_user_pages : Chained('authd_user_base') PathPart('pages') Args(0) ActionClass('FromPSGI') {
+    my ($self, $c) = @_;
+    return $self->wm(
+        $c, 'Judoon::API::Resource::Pages', {
+            set      => $c->stash->{authd_user}->pages_rs,
+            writable => 0,
+        }
+    );
+}
+
+
+
+=head1 Primary Resource Routes
+
+These are the routes for our primary data structures, Datasets and
+Pages.  These routes will have different methods available to them
+depending upon whether the user is logged in, and if the logged-in
+user owns the requested resource id.
+
+ GET requires $is_public || ($authd && $authd->owns($ds_id))
+ POST PUT DELETE requires $authd && $authd->owns($ds_id)
+
+ /datasets *XXX NO! XXX*
+ /datasets/$ds_id (GET,PUT,DELETE)
+ /datasets/$ds_id/columns (GET, POST, DELETE)
+ /datasets/$ds_id/columns/$dscol_id (GET,PUT,DELETE) (not really delete)
+ /datasets/$ds_id/pages (GET)
+ /datasets/$ds_id/data (GET)
+
+ GET requires $is_public || ($authd && $authd->owns($ds_id))
+ POST PUT DELETE requires $authd && $authd->owns($ds_id)
+
+ /pages *XXX NO! XXX*
+ /pages/$page_id (GET, PUT, DELETE)
+ /pages/$page_id/columns (GET, POST, DELETE)
+ /pages/$page_id/columns/$dscol_id (GET, PUT, DELETE)
+
+=head2 dataset_base / datasets / dataset_id / dataset
+
+
+=cut
+
+sub mixed_base : Chained('wm_base') PathPart('') CaptureArgs(0) {}
+
+sub dataset_base : Chained('mixed_base') PathPart('datasets') CaptureArgs(0) {}
+sub datasets : Chained('dataset_base') PathPart('') Args(0) {
+    my ($self, $c) = @_;
+    $self->go_here('/api/wm/public_datasets');
+    $c->detach;
+}
 sub dataset_id : Chained('dataset_base') PathPart('') CaptureArgs(1) {
     my ($self, $c, $id) = @_;
-
+    $id //= '';
     $c->stash->{dataset_id}     = $id;
-    $c->stash->{dataset_object} = $c->stash->{dataset_rs}->find({id => $id});
+    $c->stash->{dataset_object} = $c->model('User::Dataset')->find({id => $id});
+
+    if (not $c->stash->{dataset_object}) {
+        http_throw('NotFound');
+    }
+
+    my $is_public = !$c->stash->{dataset_object}->is_private;
+    $c->stash->{authd_owns} = $c->stash->{authd_user}
+        && $c->stash->{authd_user}->datasets_rs->search({id => $id})->count;
+
+    http_throw('NotFound')
+        unless ($is_public || $c->stash->{authd_owns});
 }
 sub dataset : Chained('dataset_id') PathPart('') Args(0) ActionClass('FromPSGI') {
     my ($self, $c) = @_;
     return $self->wm(
         $c, 'Judoon::API::Resource::Dataset', {
-            item     => $c->stash->{dataset_object},
-            writable => 0,
+            item      => $c->stash->{dataset_object},
+            writable  => $c->stash->{authd_owns},
         }
     );
 }
@@ -145,8 +263,8 @@ sub dscols : Chained('dscol_base') PathPart('') Args(0) ActionClass('FromPSGI') 
     my ($self, $c) = @_;
     return $self->wm(
         $c, 'Judoon::API::Resource::DatasetColumns', {
-            set      => $c->stash->{dscol_rs},
-            writable => 0,
+            set       => $c->stash->{dscol_rs},
+            writable  => $c->stash->{authd_owns},
         }
     );
 }
@@ -156,8 +274,8 @@ sub dscol : Chained('dscol_base') PathPart('') Args(1) ActionClass('FromPSGI') {
         ? $c->stash->{dscol_rs}->find($id) : undef;
     return $self->wm(
         $c, 'Judoon::API::Resource::DatasetColumn', {
-            item     => $item,
-            writable => 0,
+            item      => $item,
+            writable  => $c->stash->{authd_owns},
         }
     );
 }
@@ -173,11 +291,24 @@ sub ds_page : Chained('dataset_id') PathPart('pages') Args(0) ActionClass('FromP
     my ($self, $c) = @_;
     return $self->wm(
         $c, 'Judoon::API::Resource::Pages', {
-            set      => $c->stash->{dataset_object}->pages_ordered,
-            writable => 0,
+            set       => $c->stash->{dataset_object}->pages_ordered,
+            writable  => 0,
         }
     );
 }
+
+
+=head2 ds_data()
+
+Get the data the given dataset.
+
+=cut
+
+sub ds_data : Chained('dataset_id') PathPart('data') Args(0) ActionClass('FromPSGI') {
+    my ($self, $c) = @_;
+    http_throw(NotImplemented => {message => 'Fitz is still working on this'});
+}
+
 
 
 =head2 page_base / pages / page_id / page
@@ -187,30 +318,35 @@ to public pages.
 
 =cut
 
-sub page_base : Chained('wm_base') PathPart('pages') CaptureArgs(0) {
-    my ($self, $c) = @_;
-    $c->stash->{page_rs} = $c->model('User::Page')->public();
-}
+sub page_base : Chained('mixed_base') PathPart('pages') CaptureArgs(0) {}
 sub pages : Chained('page_base') PathPart('') Args(0) ActionClass('FromPSGI') {
     my ($self, $c) = @_;
-    return $self->wm(
-        $c, 'Judoon::API::Resource::Pages', {
-            set      => $c->stash->{page_rs},
-            writable => 0,
-        }
-    );
+    $self->go_here('/api/wm/public_pages');
+    $c->detach;
 }
 sub page_id : Chained('page_base') PathPart('') CaptureArgs(1) {
     my ($self, $c, $id) = @_;
+    $id //= '';
     $c->stash->{page_id}     = $id;
-    $c->stash->{page_object} = $c->stash->{page_rs}->find({id => $id});
+    $c->stash->{page_object} = $c->model('User::Page')->find({id => $id});
+
+    if (not $c->stash->{page_object}) {
+        http_throw('NotFound');
+    }
+
+    my $is_public = !$c->stash->{page_object}->is_private;
+    $c->stash->{authd_owns}      = $c->stash->{authd_user}
+        && $c->stash->{authd_user}->pages_rs->search({id => $id})->count;
+
+    http_throw('NotFound')
+        unless ($is_public || $c->stash->{authd_owns});
 }
 sub page : Chained('page_id') PathPart('') Args(0) ActionClass('FromPSGI') {
     my ($self, $c, $id) = @_;
     return $self->wm(
         $c, 'Judoon::API::Resource::Page', {
-            item     => $c->stash->{page_object},
-            writable => 0,
+            item      => $c->stash->{page_object},
+            writable  => $c->stash->{authd_owns},
         }
     );
 }
@@ -232,8 +368,8 @@ sub pagecols : Chained('pagecol_base') PathPart('') Args(0) ActionClass('FromPSG
     my ($self, $c) = @_;
     return $self->wm(
         $c, 'Judoon::API::Resource::PageColumns', {
-            set      => $c->stash->{pagecol_rs},
-            writable => 0,
+            set       => $c->stash->{pagecol_rs},
+            writable  => $c->stash->{authd_owns},
         }
     );
 }
@@ -243,8 +379,8 @@ sub pagecol : Chained('pagecol_base') PathPart('') Args(1) ActionClass('FromPSGI
         ? $c->stash->{pagecol_rs}->find({id => $id}) : undef;
     return $self->wm(
         $c, 'Judoon::API::Resource::PageColumn', {
-            item     => $item,
-            writable => 0,
+            item      => $item,
+            writable  => $c->stash->{authd_owns},
         }
     );
 }
