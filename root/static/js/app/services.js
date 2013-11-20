19a0af4098d49b9548ada3db3cb80039aefdfd11
Fitz Elliott <felliott@virginia.edu>
2013-11-20 10:51:49 -0500

    [jsapp] add new set of models to services.js

--- root/static/js/app/services.js
+++ root/static/js/app/services.js
@@ -122,3 +122,242 @@ judoonSrv.service('Alerts', [function() {
     this.closeAlert   = closeAlertAt;
 }]);
 
+
+judoonSrv.factory('Userp', ['$http', 'Datasetp', function($http, Datasetp) {
+    var wrapper = {
+        getDatasets: function() {
+            var future,
+                _this = this;
+            future = $http.get('/api/user/datasets');
+            return future.then(function(response) {
+                _this.datasets = [];
+                angular.forEach(response.data, function(value) {
+                    _this.datasets.push(Datasetp._buildDataset(value));
+                });
+                return _this.datasets;
+            });
+        },
+        createDataset: function(content) {
+            var future,
+                _this = this;
+            future = $http.post('/api/user/datasets', {
+                post: {
+                    content: content
+                }
+            });
+            return future.then(function(response) {
+                var post = response.data;
+                _this.datasets.push(post);
+                return post;
+            });
+        },
+        deleteDataset: function(dataset) {
+            var _this = this;
+            dataset.deleteMe().then(function() {
+                var idx = _this.datasets.indexOf( dataset );
+                _this.datasets.splice(idx, 1);
+                return dataset;
+            });
+        },
+        selectedDataset: null,
+        setSelectedDataset: function (dataset) {
+            var _this = this,
+                idx   = _this.datasets.indexOf(dataset);
+            _this.selectedDataset = _this.datasets[idx];
+
+        }
+    };
+
+
+
+    return {
+        get: function() {
+            var future,
+                _this = this;
+            future = $http.get('/api/user');
+            return future.then(
+                function(response) { return _this._buildUser(response.data); }
+            )
+            .then(
+                function(_this) {
+                    _this.getDatasets().then(
+                        function(datasets) {
+                            angular.forEach(datasets, function(value) {
+                                value.getColumns();
+                                value.getPages().then(
+                                    function(pages) {
+                                        angular.forEach(pages, function(value) {
+                                            value.getColumns();
+                                        });
+                                    }
+                                );
+                            });
+                        }
+                    );
+                    return _this;
+                }
+            );
+        },
+        _buildUser: function(user) {
+            _.extend(user, wrapper);
+            return user;
+        }
+    };
+}]);
+
+
+judoonSrv.factory(
+    'Datasetp',
+    ['$http', 'Pagesp', 'DatasetColumnsp', function($http, Pagesp, DatasetColumnsp) {
+    var wrapper = {
+        deleteMe: function() {
+            var future,
+                _this = this;
+            future = $http.delete('/api/datasets/' + _this.id);
+            return future;
+        },
+        getColumns: function() {
+            var future,
+                _this = this;
+            future = $http.get('/api/datasets/' + _this.id + '/columns');
+            return future.then(function(response) {
+                _this.columns = [];
+                angular.forEach(response.data, function(value) {
+                    _this.columns.push(DatasetColumnsp._buildDatasetColumn(value));
+                });
+                return _this.columns;
+            });
+        },
+        getPages: function() {
+            var future,
+                _this = this;
+            future = $http.get('/api/datasets/' + _this.id + '/pages');
+            return future.then(function(response) {
+                _this.pages = [];
+                angular.forEach(response.data, function(value) {
+                    _this.pages.push(Pagesp._buildPage(value));
+                });
+                return _this.pages;
+            });
+        },
+        createPage: function (newPage) {
+            var _this = this;
+            newPage.dataset_id = _this.id;
+            return $http.post('/api/user/pages', newPage)
+                .success( function(data, status, getHeader) {
+                    $http.get(getHeader('Location')).success(cb);
+                });
+        }
+    };
+    return {
+        get: function(datasetId) {
+            var future,
+                _this = this;
+            future = $http.get('/api/datasets/' + datasetId);
+            return future.then(
+                function(response) { return _this._buildDataset(response.data); }
+            )
+            .then(
+                function(_this) {
+                    //_this.getPages();
+                    _this.getColumns();
+                    return _this;
+                }
+            );
+        },
+        _buildDataset: function(dataset) {
+            _.extend(dataset, wrapper);
+            return dataset;
+        }
+    };
+    }]
+);
+
+
+judoonSrv.factory(
+    'DatasetColumnsp', ['$http', function($http) {
+        var wrapper = {
+        };
+        return {
+            get: function(datasetId, datasetColId) {
+                var future,
+                    _this = this;
+                future = $http.get('/api/datasets/' + datasetId + '/columns/'
+                                   + datasetColId);
+                return future.then(
+                    function(response) {
+                        return _this._buildDatasetColumn(response.data);
+                    }
+                );
+            },
+            _buildDatasetColumn: function(datasetcolumn) {
+                _.extend(datasetcolumn, wrapper);
+                return datasetcolumn;
+            }
+        };
+    }]
+);
+
+
+
+judoonSrv.factory(
+    'Pagesp',
+    ['$http', 'PageColumnsp', function($http, PageColumnsp) {
+    var wrapper = {
+        getColumns: function() {
+            var future,
+                _this = this;
+            future = $http.get('/api/pages/' + _this.id + '/columns');
+            return future.then(function(response) {
+                _this.columns = [];
+                angular.forEach(response.data, function(value) {
+                    _this.columns.push(PageColumnsp._buildPageColumn(value));
+                });
+                return _this.columns;
+            });
+        }
+    };
+    return {
+        get: function(pageId) {
+            var future,
+                _this = this;
+            future = $http.get('/api/pages/' + pageId);
+            return future.then(
+                function(response) { return _this._buildPage(response.data); }
+            )
+            .then(
+                function(_this) { _this.getColumns(); return _this; }
+            );
+        },
+        _buildPage: function(page) {
+            _.extend(page, wrapper);
+            return page;
+        }
+    };
+    }]
+);
+
+
+judoonSrv.factory(
+    'PageColumnsp', ['$http', function($http) {
+        var wrapper = {
+        };
+        return {
+            get: function(pageId, pageColId) {
+                var future,
+                    _this = this;
+                future = $http.get('/api/pages/' + pageId + '/columns/'
+                                   + pageColId);
+                return future.then(
+                    function(response) {
+                        return _this._buildPageColumn(response.data);
+                    }
+                );
+            },
+            _buildPageColumn: function(pagecolumn) {
+                _.extend(pagecolumn, wrapper);
+                return pagecolumn;
+            }
+        };
+    }]
+);
