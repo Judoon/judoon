506d79e920c94a9c81cd47825348d5fdfd9deb2e
Fitz Elliott <felliott@virginia.edu>
2013-11-25 13:31:22 -0500

    [jsapp] move dataTable directive template into its own file

--- root/static/js/app/directives.js
+++ root/static/js/app/directives.js
@@ -13,20 +13,12 @@ var judoonDir = angular.module('judoon.directives', []);
 **/
 
 judoonDir.directive('judoonDataTable', ['$timeout', function($timeout) {
-    var dataTableTemplate = '<table class="table table-striped table-condensed table-bordered">' +
-        '<thead><tr>' +
-        '<th ng-class="{highlight: highlightActive({column: column}), \'highlight-danger\': highlightDelete({column: column})}"' +
-           ' ng-repeat="column in columns">{{ column[headerKey] }}</th>' +
-        '</tr></thead>' +
-        '<tbody></tbody>' +
-        '</table>';
-
-
     return {
-        restrict: 'E',
-        replace: true,
-        template: dataTableTemplate,
-        scope: {
+        restrict    : 'E',
+        replace     : true,
+        templateUrl : '/static/html/partials/judoon-data-table.html',
+        transclude  : false,
+        scope       : {
             datasetId       : '=jdtDatasetId',
             columns         : '=jdtColumns',
             headerKey       : '@jdtHeaderKey',
