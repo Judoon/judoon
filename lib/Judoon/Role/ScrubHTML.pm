37c570f1d12ce6e7f4a004788fbbbf3038742696
Fitz Elliott <felliott@virginia.edu>
2013-12-10 14:05:26 -0500

    [scrubhtml] update with all tags from MDN categorized
     * I went through the list of tags at
       https://developer.mozilla.org/en-US/docs/Web/HTML/Element and
       categorized them all into groups. Those groups can be found in the
       source of ScrubHTML.pm.  We allow/deny certain tags based upon the
       group they're in.

--- lib/Judoon/Role/ScrubHTML.pm
+++ lib/Judoon/Role/ScrubHTML.pm
@@ -47,6 +47,41 @@ elements.
 
 =cut
 
+
+# see cmc-notes.org: Valid / Invalid HTML Tags
+# valid
+my @inline_tags = qw(
+    a abbr b bdi bdo cite code del dfn em i ins kbd mark meter q s
+    samp small span strong sub sup time u var wbr
+);
+my @block_tags = qw(
+    address article aside blockquote br caption col colgroup dd details
+    div dl dt figcaption figure footer h1 h2 h3 h4 h5 h6 header hr img li
+    nav ol p pre section summary table tbody td tfoot th thead tr ul
+);
+
+# maybe valid one day
+my @advanced_tags = qw(
+    area audio canvas content data decorator element embed
+    keygen main map menu menuitem object param progress
+    rp rt ruby shadow source template track video
+);
+my @form_tags = qw(
+    button datalist fieldset form input label legend
+    optgroup option output select textarea
+);
+
+#invalid
+my @structure_tags = qw(body head html);
+my @head_tags      = qw(base link meta title);
+my @invalid_tags   = qw(iframe noscript script style);
+my @obsolete_tags  = qw(
+    acronym applet basefont bgsound big blink center dir
+    font frame frameset hgroup isindex listing marquee
+    nobr noframes plaintext spacer strike tt xmp
+);
+
+
 has html_block_scrubber => (
     is  => 'lazy',
     isa => InstanceOf['HTML::Scrubber'],
@@ -54,15 +89,12 @@ has html_block_scrubber => (
 sub _build_html_block_scrubber {
     my ($self) = @_;
     my $scrubber = HTML::Scrubber->new;
+    $scrubber->{_p}->closing_plaintext(1);
     $scrubber->rules(
-        strong     => 1,
-        em         => 1,
+        (map {$_ => 1} @inline_tags, @block_tags),
         p          => {style => 1,},
-        sub        => 1,
-        sup        => 1,
         span       => {style => 1,},
         a          => {href => 1, target => 1,},
-        blockquote => 1,
         table      => {style => 1,},
         thead      => {style => 1,},
         tbody      => {style => 1,},
@@ -70,18 +102,8 @@ sub _build_html_block_scrubber {
         tr         => {style => 1,},
         th         => {style => 1, colspan => 1, rowspan => 1,},
         td         => {style => 1, colspan => 1, rowspan => 1,},
-        ol         => 1,
-        ul         => 1,
-        li         => 1,
-        hr         => 1,
-        h1         => 1,
-        h2         => 1,
-        h3         => 1,
-        h4         => 1,
-        h5         => 1,
-        h6         => 1,
-        pre        => 1,
-        address    => 1,
+        (map {$_ => 0} @structure_tags, @head_tags, @invalid_tags,
+         @obsolete_tags, @advanced_tags, @form_tags),
     );
     return $scrubber;
 }
@@ -102,14 +124,14 @@ has html_string_scrubber => (
 sub _build_html_string_scrubber {
     my ($self) = @_;
     my $scrubber = HTML::Scrubber->new;
+    $scrubber->{_p}->closing_plaintext(1);
     $scrubber->rules(
-        strong => 1,
-        em     => 1,
-        p      => 0,
-        sub    => 1,
-        sup    => 1,
+        (map {$_ => 1} @inline_tags),
         span   => {style => 1,},
         a      => {href => 1, target => 1,},
+        (map {$_ => 0} @structure_tags, @head_tags, @invalid_tags,
+         @obsolete_tags, @advanced_tags, @form_tags, @block_tags),
+
     );
     return $scrubber;
 }
--- t/lib/t/Role/HtmlFixtures.pm
+++ t/lib/t/Role/HtmlFixtures.pm
@@ -26,7 +26,9 @@ my @fixture_types = qw(tainted scrubbed_string scrubbed_block);
 my @fixture_sets  = qw(
     basic basic_entity simple_html scary_html simple_block
     encoded_scary long_block
+    allinlinetags allblocktags badtags
 );
+
 
 
 =head1 ATTRIBUTES
@@ -131,18 +133,180 @@ hey
 <strong>Mogilner A</strong> , Edelstein-Keshet L. Regulation of actin dynamics in rapidly moving cells: a quantitative analysis. Biophys J. 2002;83(3):1237-58.
 @@ long_block-scrubbed_block
 <p><strong>Mogilner A</strong> , Edelstein-Keshet L. Regulation of actin dynamics in rapidly moving cells: a quantitative analysis. Biophys J. 2002;83(3):1237-58.</p>
-@@ placeholder-tainted
-@@ placeholder-scrubbed_string
-@@ placeholder-scrubbed_block
-@@ placeholder-tainted
-@@ placeholder-scrubbed_string
-@@ placeholder-scrubbed_block
-@@ placeholder-tainted
-@@ placeholder-scrubbed_string
-@@ placeholder-scrubbed_block
-@@ placeholder-tainted
-@@ placeholder-scrubbed_string
-@@ placeholder-scrubbed_block
+@@ allinlinetags-tainted
+<a>a</a><abbr>abbr</abbr> <b>b</b> <bdi>bdi</bdi> <bdo>bdo</bdo>
+<cite>cite</cite> <code>code</code> <del>del</del> <dfn>dfn</dfn>
+<em>em</em> <i>i</i> <ins>ins</ins> <kbd>kbd</kbd> <mark>mark</mark>
+<meter>meter</meter> <q>q</q> <s>s</s> <samp>samp</samp>
+<small>small</small> <span>span</span> <strong>strong</strong>
+<sub>sub</sub> <sup>sup</sup> <time>time</time> <u>u</u>
+<var>var</var> <wbr>
+@@ allinlinetags-scrubbed_string
+<a>a</a><abbr>abbr</abbr> <b>b</b> <bdi>bdi</bdi> <bdo>bdo</bdo>
+<cite>cite</cite> <code>code</code> <del>del</del> <dfn>dfn</dfn>
+<em>em</em> <i>i</i> <ins>ins</ins> <kbd>kbd</kbd> <mark>mark</mark>
+<meter>meter</meter> <q>q</q> <s>s</s> <samp>samp</samp>
+<small>small</small> <span>span</span> <strong>strong</strong>
+<sub>sub</sub> <sup>sup</sup> <time>time</time> <u>u</u>
+<var>var</var> <wbr>
+@@ allinlinetags-scrubbed_block
+<a>a</a><abbr>abbr</abbr> <b>b</b> <bdi>bdi</bdi> <bdo>bdo</bdo>
+<cite>cite</cite> <code>code</code> <del>del</del> <dfn>dfn</dfn>
+<em>em</em> <i>i</i> <ins>ins</ins> <kbd>kbd</kbd> <mark>mark</mark>
+<meter>meter</meter> <q>q</q> <s>s</s> <samp>samp</samp>
+<small>small</small> <span>span</span> <strong>strong</strong>
+<sub>sub</sub> <sup>sup</sup> <time>time</time> <u>u</u>
+<var>var</var> <wbr>
+@@ allblocktags-tainted
+<a>a</a><abbr>abbr</abbr> <b>b</b> <bdi>bdi</bdi> <bdo>bdo</bdo>
+<cite>cite</cite> <code>code</code> <del>del</del> <dfn>dfn</dfn>
+<em>em</em> <i>i</i> <ins>ins</ins> <kbd>kbd</kbd> <mark>mark</mark>
+<meter>meter</meter> <q>q</q> <s>s</s> <samp>samp</samp>
+<small>small</small> <span>span</span> <strong>strong</strong>
+<sub>sub</sub> <sup>sup</sup> <time>time</time> <u>u</u>
+<var>var</var> <wbr>
+
+<address>address</address> <article>article</article>
+<aside>aside</aside> <blockquote>blockquote</blockquote> <br>br</br>
+<caption>caption</caption> <col>col</col>
+<colgroup>colgroup</colgroup> <dd>dd</dd> <details>details</details>
+<div>div</div> <dl>dl</dl> <dt>dt</dt>
+<figcaption>figcaption</figcaption> <figure>figure</figure>
+<footer>footer</footer> <h1>h1</h1> <h2>h2</h2> <h3>h3</h3>
+<h4>h4</h4> <h5>h5</h5> <h6>h6</h6> <header>header</header>
+<hr>hr</hr> <img>img</img> <li>li</li> <nav>nav</nav> <ol>ol</ol>
+<p>p</p> <pre>pre</pre> <section>section</section>
+<summary>summary</summary> <table>table</table> <tbody>tbody</tbody>
+<td>td</td> <tfoot>tfoot</tfoot> <th>th</th> <thead>thead</thead>
+<tr>tr</tr> <ul>ul</ul>
+@@ allblocktags-scrubbed_string
+<a>a</a><abbr>abbr</abbr> <b>b</b> <bdi>bdi</bdi> <bdo>bdo</bdo>
+<cite>cite</cite> <code>code</code> <del>del</del> <dfn>dfn</dfn>
+<em>em</em> <i>i</i> <ins>ins</ins> <kbd>kbd</kbd> <mark>mark</mark>
+<meter>meter</meter> <q>q</q> <s>s</s> <samp>samp</samp>
+<small>small</small> <span>span</span> <strong>strong</strong>
+<sub>sub</sub> <sup>sup</sup> <time>time</time> <u>u</u>
+<var>var</var> <wbr>
+
+address article
+aside blockquote br
+caption col
+colgroup dd details
+div dl dt
+figcaption figure
+footer h1 h2 h3
+h4 h5 h6 header
+hr img li nav ol
+p pre section
+summary table tbody
+td tfoot th thead
+tr ul
+@@ allblocktags-scrubbed_block
+<a>a</a><abbr>abbr</abbr> <b>b</b> <bdi>bdi</bdi> <bdo>bdo</bdo>
+<cite>cite</cite> <code>code</code> <del>del</del> <dfn>dfn</dfn>
+<em>em</em> <i>i</i> <ins>ins</ins> <kbd>kbd</kbd> <mark>mark</mark>
+<meter>meter</meter> <q>q</q> <s>s</s> <samp>samp</samp>
+<small>small</small> <span>span</span> <strong>strong</strong>
+<sub>sub</sub> <sup>sup</sup> <time>time</time> <u>u</u>
+<var>var</var> <wbr>
+
+<address>address</address> <article>article</article>
+<aside>aside</aside> <blockquote>blockquote</blockquote> <br>br</br>
+<caption>caption</caption> <col>col</col>
+<colgroup>colgroup</colgroup> <dd>dd</dd> <details>details</details>
+<div>div</div> <dl>dl</dl> <dt>dt</dt>
+<figcaption>figcaption</figcaption> <figure>figure</figure>
+<footer>footer</footer> <h1>h1</h1> <h2>h2</h2> <h3>h3</h3>
+<h4>h4</h4> <h5>h5</h5> <h6>h6</h6> <header>header</header>
+<hr>hr</hr> <img>img</img> <li>li</li> <nav>nav</nav> <ol>ol</ol>
+<p>p</p> <pre>pre</pre> <section>section</section>
+<summary>summary</summary> <table>table</table> <tbody>tbody</tbody>
+<td>td</td> <tfoot>tfoot</tfoot> <th>th</th> <thead>thead</thead>
+<tr>tr</tr> <ul>ul</ul>
+@@ badtags-tainted
+<html><head><base><link><meta><title>quack</title></head><body>
+<p>invalid</p>
+<script>script</script><noscript>noscript</noscript><iframe>iframe</iframe><style>style</style>
+<p>obsolete</p>
+<acronym>acronym</acronym><applet>applet</applet><basefont>basefont</basefont>
+<bgsound>bgsound</bgsound><big>big</big><blink>blink</blink><center>center</center>
+<dir>dir</dir><font>font</font><frame>frame</frame><frameset>frameset</frameset>
+<hgroup>hgroup</hgroup><isindex>isindex</isindex><listing>listing</listing>
+<plaintext>plaintext</plaintext><marquee>marquee</marquee><nobr>nobr</nobr><noframes>noframes</noframes>
+<spacer>spacer</spacer><strike>strike</strike>
+<tt>tt</tt><xmp>xmp</xmp>
+<p>advanced</p>
+<area>area</area><audio>audio</audio><canvas>canvas</canvas>
+<content>content</content><data>data</data><decorator>decorator</decorator>
+<element>element</element><embed>embed</embed><keygen>keygen</keygen>
+<main>main</main><map>map</map><menu>menu</menu><menuitem>menuitem</menuitem>
+<object>object</object><param>param</param><progress>progress</progress>
+<rp>rp</rp><rt>rt</rt><ruby>ruby</ruby><shadow>shadow</shadow>
+<source>source</source><template>template</template><track>track</track>
+<video>video</video>
+<p>form</p>
+<button>button</button><datalist>datalist</datalist>
+<fieldset>fieldset</fieldset><form>form</form><input>input</input>
+<label>label</label><legend>legend</legend><optgroup>optgroup</optgroup>
+<option>option</option><output>output</output><select>select</select>
+<textarea>textarea</textarea>
+</body></html>
+@@ badtags-scrubbed_string
+quack
+invalid
+noscriptiframe
+obsolete
+acronymappletbasefont
+bgsoundbigblinkcenter
+dirfontframeframeset
+hgroupisindexlisting
+plaintextmarqueenobrnoframes
+spacerstrike
+ttxmp
+advanced
+areaaudiocanvas
+contentdatadecorator
+elementembedkeygen
+mainmapmenumenuitem
+objectparamprogress
+rprtrubyshadow
+sourcetemplatetrack
+video
+form
+buttondatalist
+fieldsetforminput
+labellegendoptgroup
+optionoutputselect
+textarea
+
+@@ badtags-scrubbed_block
+quack
+<p>invalid</p>
+noscriptiframe
+<p>obsolete</p>
+acronymappletbasefont
+bgsoundbigblinkcenter
+dirfontframeframeset
+hgroupisindexlisting
+plaintextmarqueenobrnoframes
+spacerstrike
+ttxmp
+<p>advanced</p>
+areaaudiocanvas
+contentdatadecorator
+elementembedkeygen
+mainmapmenumenuitem
+objectparamprogress
+rprtrubyshadow
+sourcetemplatetrack
+video
+<p>form</p>
+buttondatalist
+fieldsetforminput
+labellegendoptgroup
+optionoutputselect
+textarea
+
 @@ placeholder-tainted
 @@ placeholder-scrubbed_string
 @@ placeholder-scrubbed_block
