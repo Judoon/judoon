34608ec55236d568fa7a1b907deb79e45497200e
Fitz Elliott <felliott@virginia.edu>
2013-12-11 14:22:46 -0500

    [tmpl] document varstring_type and accession attrs in VarString

--- lib/Judoon/Tmpl/Node/VarString.pm
+++ lib/Judoon/Tmpl/Node/VarString.pm
@@ -1,5 +1,40 @@
 package Judoon::Tmpl::Node::VarString;
 
+=pod
+
+=encoding utf-8
+
+=for stopwords linkbuilder
+
+=head1 NAME
+
+Judoon::Tmpl::Node::VarString - Represents mixed static/variable text
+
+=head1 DESCRIPTION
+
+A L<Judoon::Tmpl::Node::VarString> is not a node that is inserted
+directly by the user.  Rather it is a sub-component of other Node
+types.  It represents a string with both static and variable parts.
+The static parts are stored in the C<text_segments> attribute, while
+the variable parts are stored in the C<variable_segments> attribute.
+
+When the C<VarString> is being decomposed, it is always assumed that
+the first base node will come from the C<text_segments> attribute. So,
+for example, given a C<VarString> with the following contents:
+
+ $self->text_segments(['foo','baz']);
+ $self->variable_segments(['bar','quux']);
+
+...the following javascript template string would be produced:
+
+ foo{{bar}}baz{{quux}}
+
+If the expression represented by a C<VarString> begins with a
+variable, then the first element of C<text_segments> should be the
+empty string.
+
+=cut
+
 use Moose;
 use namespace::autoclean;
 
@@ -14,12 +49,42 @@ use Moose::Util::TypeConstraints qw(enum);
 
 enum 'VarStringType', [qw(static variable accession)];
 
+=head1 ATTRIBUTES
+
+=head2 type
+
+The type is C<varstring>.
+
+=head2 varstring_type
+
+One of C<static>, C<variable>, or C<accession>.  This is
+meta-information that we need to easily populate the linkbuilder form
+widget on the website.  If C<accession>, the name of the site being
+linked to is stored in the C<accession> attribute.
+
+=head2 accession
+
+The name of the site being linked to.
+
+=head2 text_segments
+
+A list of strings that will be decomposed into plain C<Text> nodes.
+
+=head2 variable_segments
+
+A list of variable names that will be decomposed into C<Variable>
+nodes.
+
+=cut
+
 has '+type' => (default => 'varstring',);
 has varstring_type    => (is => 'ro', isa => 'VarStringType', required => 1, );
 has text_segments     => (is => 'ro', isa => 'ArrayRef[Str]', required => 1, );
 has variable_segments => (is => 'ro', isa => 'ArrayRef[Str]', required => 1, );
 has accession         => (is => 'ro', isa => 'Str', default => '',);
 
+
+=head1 METHODS
 
 =head2 decompose()
 
