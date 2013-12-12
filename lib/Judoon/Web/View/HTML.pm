231df737ae35fe787876022dd19faab2d12121d9
Fitz Elliott <felliott@virginia.edu>
2013-12-12 10:06:48 -0500

    [web] add uri_for_action to view methods, stop using c in templates

--- lib/Judoon/Web/View/HTML.pm
+++ lib/Judoon/Web/View/HTML.pm
@@ -22,7 +22,24 @@ extends 'Catalyst::View::TT';
 __PACKAGE__->config(
     TEMPLATE_EXTENSION => '.tt',
     render_die => 1,
+    expose_methods     => [qw(uri_for_action)],
 );
+
+
+=head1 METHODS
+
+=head2 uri_for_action
+
+Provide the C<uri_for_action> context object method to the template.
+
+=cut
+
+sub uri_for_action {
+    my $self   = shift;
+    my $c      = shift;
+    my $action = shift;
+    return $c->uri_for_action("$action", @_);
+}
 
 
 =head1 SEE ALSO
