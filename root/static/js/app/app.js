5281c9377403c94152f5360688c8ce5b506a2bdc
Fitz Elliott <felliott@virginia.edu>
2013-12-19 12:43:41 -0500

    [jsapp] add basic loading widget to top of page
     * code from:
       http://codingsmackdown.tv/blog/2013/04/20/using-response-interceptors-to-show-and-hide-a-loading-widget-redux/

--- root/static/js/app/app.js
+++ root/static/js/app/app.js
@@ -9,9 +9,14 @@ var judoonApp = angular.module(
      'judoon.services', 'judoon.controllers', 'judoon.directives']
 );
 
+judoonApp.constant('_START_REQUEST_', '_START_REQUEST_');
+judoonApp.constant('_END_REQUEST_', '_END_REQUEST_');
+
 judoonApp.config(
-    ['$locationProvider', '$routeProvider',
-     function($locationProvider, $routeProvider) {
+    ['$locationProvider', '$routeProvider', '$httpProvider',
+     '_START_REQUEST_', '_END_REQUEST_',
+     function($locationProvider, $routeProvider, $httpProvider,
+              _START_REQUEST_, _END_REQUEST_) {
 
          $locationProvider.html5Mode(true);
          $routeProvider
@@ -59,6 +64,47 @@ judoonApp.config(
                  }
              })
              .otherwise({redirectTo: '/'});
+
+         var $http,
+             interceptor = ['$q', '$injector', function ($q, $injector) {
+                 var rootScope;
+
+                 function success(response) {
+                     // get $http via $injector because of circular dependency problem
+                     $http = $http || $injector.get('$http');
+                     // don't send notification until all requests are complete
+                     if ($http.pendingRequests.length < 1) {
+                         // get $rootScope via $injector because of circular dependency problem
+                         rootScope = rootScope || $injector.get('$rootScope');
+                         // send a notification requests are complete
+                         rootScope.$broadcast(_END_REQUEST_);
+                     }
+                     return response;
+                 }
+
+                 function error(response) {
+                     // get $http via $injector because of circular dependency problem
+                     $http = $http || $injector.get('$http');
+                     // don't send notification until all requests are complete
+                     if ($http.pendingRequests.length < 1) {
+                         // get $rootScope via $injector because of circular dependency problem
+                         rootScope = rootScope || $injector.get('$rootScope');
+                         // send a notification requests are complete
+                         rootScope.$broadcast(_END_REQUEST_);
+                     }
+                     return $q.reject(response);
+                 }
+
+                 return function (promise) {
+                     // get $rootScope via $injector because of circular dependency problem
+                     rootScope = rootScope || $injector.get('$rootScope');
+                     // send notification a request has started
+                     rootScope.$broadcast(_START_REQUEST_);
+                     return promise.then(success, error);
+                 };
+             }];
+
+         $httpProvider.responseInterceptors.push(interceptor);
      }
     ]
 );
--- root/static/js/app/directives.js
+++ root/static/js/app/directives.js
@@ -115,6 +115,33 @@ judoonDir.directive(
     }]
 );
 
+
+judoonDir.directive(
+    'loadingWidget',
+    ['_START_REQUEST_', '_END_REQUEST_',
+     function (_START_REQUEST_, _END_REQUEST_) {
+         return {
+             restrict: "A",
+             link: function (scope, element) {
+                 // hide the element initially
+                 element.hide();
+
+                 scope.$on(_START_REQUEST_, function () {
+                     // got the request start notification, show the element
+                     element.show();
+                 });
+
+                 scope.$on(_END_REQUEST_, function () {
+                     // got the request end notification, hide the element
+                     element.hide();
+                 });
+             }
+         };
+     }
+    ]
+);
+
+
 judoonDir.directive(
     'judoonWidgetFactory',
     ['$compile', function($compile) {
